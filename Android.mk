df6f08ba783443933917524e728a575f680842ea
HyejungKwon <cjscld15@nexell.co.kr>
2019-05-29 10:09:13 +0900

    add makefile for Android
    this patch adds makefile for Android
    
    Signed-off-by: HyejungKwon <cjscld15@nexell.co.kr>
    Change-Id: I56b0ba20b5b9889ba28cba694ab24e37cdd78147
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_library_nx-v4l2/+/456105
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>
    Tested-by: Sungwoo Park <swpark@nexell.co.kr>
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_library_nx-v4l2/+/461516
    Reviewed-by: allan park <allan.park@nexell.co.kr>
    Tested-by: allan park <allan.park@nexell.co.kr>

---
+++ Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libnx_v4l2
+
+ANDROID_VERSION_STR := $(PLATFORM_VERSION)
+ANDROID_VERSION := $(firstword $(ANDROID_VERSION_STR))
+ifeq ($(ANDROID_VERSION), 9)
+LOCAL_VENDOR_MODULE := true
+endif
+
+LOCAL_SRC_FILES := \
+	nx-v4l2.c
+LOCAL_SHARED_LIBRARIES := \
+	liblog \
+	libcutils \
+	libhardware
+LOCAL_C_INCLUDES += \
+	system/core/include/utils \
+	frameworks/native/include \
+	$(call include-path-for)
+
+include $(BUILD_SHARED_LIBRARY)
